e35de16560b6efb0daa0faadb423c47952cd66f7
Muhammad Usman Rehan <90567248+Usman54321@users.noreply.github.com>
2022-10-11 15:42:30 -0400

    Initial Push

---
+++ client/start_for_windows_users.ps1
@@ -0,0 +1,24 @@
+# Evaluate node -v and check if it is greater or equal to 17.0.0
+
+$nodeVersion = node -v
+$nodeVersion = $nodeVersion -replace "v", ""
+
+# debug print out the node version
+# Write-Host "Node version: $nodeVersion"
+
+if ($nodeVersion -ge "17.0.0") {
+    $env:NODE_OPTIONS = "--openssl-legacy-provider"
+    Write-Host "Node version is greater than or equal to 17.0.0"
+}
+
+# Check if node_modules folder exists
+if (Test-Path -Path "node_modules") {
+    Write-Host "node_modules folder exists"
+} else {
+    Write-Host "node_modules folder does not exist"
+    Write-Host "Installing node_modules"
+    npm install
+}
+
+# Run npm start
+npm start
