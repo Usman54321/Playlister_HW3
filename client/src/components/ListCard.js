abfd02fb45d79858c22bea5fa319967f96053f96
Muhammad Usman Rehan <90567248+Usman54321@users.noreply.github.com>
2022-10-18 22:42:31 -0400

    if you are editing you can no longer modify playlists

--- client/src/components/ListCard.js
+++ client/src/components/ListCard.js
@@ -78,7 +78,7 @@ function ListCard(props) {
         selectClass = "selected-list-card";
     }
     let cardStatus = false;
-    if (store.isListNameEditActive) {
+    if (store.listNameActive) {
         cardStatus = true;
     }
     let cardElement =
